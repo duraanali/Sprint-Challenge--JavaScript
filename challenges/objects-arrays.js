50d9ed41aca05a737b80ba0c020f2591adf2900a
Duraan Ali <duraan522@gmail.com>
2019-06-14 10:16:56 -0500

    Started working on project

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,6 +8,18 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const dinosaursOne = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  rawr: function () {
+    return 'RAWERSRARARWERSARARARRRR';
+  }
+};
+
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
